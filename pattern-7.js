2ad1d35a5e3069634ffadbb471d8c68b6219f8fb
Anique Maniac <47491333+AniqueManiac@users.noreply.github.com>
2021-11-23 22:37:51 +0530

    Update pattern-7.js

--- pattern-7.js
+++ pattern-7.js
@@ -1,13 +1,13 @@
 var n = 5;
 string = "";
 
-for(i = 0; i<=n ; i++){
+for(i = 0; i<=n-1 ; i++){
   for(k = 0 ; k <=i ;k++){
       string += " ";
   }
 
         //i++;
-        for(j = n ; j >=i ; j--){ 
+        for(j = n-1 ; j >=i ; j--){ 
             string += "x ";
         }
    

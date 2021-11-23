a309a4ef2d7bc25d29f14f53845e06d4e7e96fd0
Anique Maniac <47491333+AniqueManiac@users.noreply.github.com>
2021-11-23 22:17:49 +0530

    Create pattern-7.js

---
+++ pattern-7.js
@@ -0,0 +1,16 @@
+var n = 5;
+string = "";
+
+for(i = 0; i<=n ; i++){
+  for(k = 0 ; k <=i ;k++){
+      string += " ";
+  }
+
+        //i++;
+        for(j = n ; j >=i ; j--){ 
+            string += "x ";
+        }
+   
+   string += "\n";
+}
+console.log(string);
